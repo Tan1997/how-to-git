e31a278c3c0cffde0695b72d04541d8c929a6736
DUT-tjy <1997tjy@mail.dlut.edu.cn>
2020-12-06 19:49:37 +0800

    test1

---
+++ 20.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'(':')', '[':']', '{':'}','a':'a'}
+        stack = ['a']
+        for i in s:
+            if i in dic:
+                stack.append(i)
+            elif dic[stack.pop()] != i:
+                return False
+    return len(stack) == 1
